7b43eb15b3688a6921369e1ef43904b7752e6f2a
Joana Hrotkó <joana.hrotko@gmail.com>
2025-07-21 21:23:17 +0100

    Fix google form section

--- src/meal/MealForm.js
+++ src/meal/MealForm.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { Copo } from "../design/Images";
 import { CONFIRM_PRESENCE, isMobile } from "../constants";
-
+import "./MealForm.css";
 import Segment from "../layout/Segment";
 
 const MealForm = () => {
@@ -16,19 +16,25 @@ const MealForm = () => {
   };
 
   return (
-    <Segment id={CONFIRM_PRESENCE.id} title={CONFIRM_PRESENCE.title} Icon={<Copo/>}>
-      <iframe
-        ref={iframeRef}
-        src="https://docs.google.com/forms/d/e/1FAIpQLSccXzWU1LRq8fN-Pt0w1ErbTVVevllB0GbNwO08QoBP1DPnzQ/viewform?embedded=true"
-        width="100%"
-        height={!isMobile ? "800" : "1000"}
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-        title="iframe-meal"
-        onLoad={handleIframeLoad}
-      />
-    </Segment>
+    <div className="meal">
+      <Segment
+        id={CONFIRM_PRESENCE.id}
+        title={CONFIRM_PRESENCE.title}
+        Icon={<Copo />}
+      >
+        <iframe
+          ref={iframeRef}
+          src="https://docs.google.com/forms/d/e/1FAIpQLSccXzWU1LRq8fN-Pt0w1ErbTVVevllB0GbNwO08QoBP1DPnzQ/viewform?embedded=true"
+          height={!isMobile ? "800" : "1000"}
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+          title="iframe-meal"
+          onLoad={handleIframeLoad}
+          // className="meal"
+        />
+      </Segment>
+    </div>
   );
 };
 
