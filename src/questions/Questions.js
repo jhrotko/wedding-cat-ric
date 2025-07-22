8d0c7d8044de944d390d889e88df5284b36c6741
Joana Hrotkó <joana.hrotko@gmail.com>
2025-07-22 21:00:11 +0100

    fix phone number

--- src/questions/Questions.js
+++ src/questions/Questions.js
@@ -15,7 +15,7 @@ const Questions = () => {
           Temos todo o gosto em ajudar com o que pudermos.
         </p>
         <ul>
-          <li>Ricardo: (+351) 918181093</li>
+          <li>Ricardo: (+351) 919181093</li>
           <li>Catarina: (+351) 966312351</li>
         </ul>
       </Segment>
